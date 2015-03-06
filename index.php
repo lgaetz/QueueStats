cf328083d9ac9cad98b0f8d7a724117ada5dd707
Lorne Gaetz <lgaetz@gmail.com>
2015-03-06 17:20:28 -0400

    Created FreePBX module

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html> 
 <head>
 <?php
-  $queue=$_GET['queue'];
- echo "<meta http-equiv='refresh' 
+$queue=$_GET['queue'];
+echo "<meta http-equiv='refresh' 
 content='5;url=index.php?queue=$queue'>";
 ?>
  <title>Advanced Recovery Systems Queue Monitor</title>
@@ -40,101 +40,100 @@ content='5;url=index.php?queue=$queue'>";
   </style>
 
 </head>
- <body>
+<body>
 <center>
 
 <?php
-  $queue=$_GET['queue'];
-  require_once('phpagi/phpagi-asmanager.php');
 
-$myfile = '/etc/asterisk/queues_additional.conf';
-echo "<form id='queue' action='index.php' method='GET'>";
-$lines = file($myfile);    
- echo "Select the queue - ";
-foreach($lines as $queues){
- if (preg_match("/]/i", $queues)) {
- echo "<button name='queue' type='submit' value='".substr($queues, 1, 
--2)."'>".substr($queues, 1, -2)."</button>";
- }
+$queue=$_GET['queue'];
+
+// load FreeBPX bootstrap environment, requires FreePBX 2.9 or higher
+if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
+	include_once('/etc/asterisk/freepbx.conf');
 }
+global $astman;
+
+// get array of all queues from asterisk. Asterisk always returns "default" as one of the queues, not sure what to do with this, keeping for now.
+if($astman->connect()) {
+	$queue_show = $astman->Command("queue show");
+	$pattern = "~(.*?) has \d*? calls~";   //regex tested and working with Asterisk 11
+	preg_match_all($pattern, $queue_show['data'], $lines);  // 2D array of queues in $lines[1]
+}
+
+echo "<form id='queue' action='index.php' method='GET'>";
+echo "Select the queue - ";
+echo "<button name='queue' type='submit' value=''>ALL</button>";
+foreach($lines[1] as $queues) {
+	echo "<button name='queue' type='submit' value='".$queues."'>".$queues."</button>";
+}
+
 echo "</form>";
 echo "<br />";
 
-  $asm = new AGI_AsteriskManager();
-  if($asm->connect())
-  {
-    $result = $asm->Command("queue show $queue");
 
-    // ECHO THE QUEUE STATUS FIRST
-
-    if(!strpos($result['data'], ':'))
-      echo $peer['data'];
-    else
-    {
-      $data = array();
-      echo "<table border='1'; cellpadding=6pt;>";
-      echo "<tr class='heading';><td>Queue Number</td><td>Calls in 
+if($astman->connect()) {
+	$result = $astman->Command("queue show $queue");
+	// ECHO THE QUEUE STATUS FIRST
+	if(!strpos($result['data'], ':')) {
+		echo $peer['data'];
+	} else {
+		$data = array();
+		echo "<table border='1'; cellpadding=6pt;>";
+		echo "<tr class='heading';><td>Queue Number</td><td>Calls in 
 queue</td><td>Answered calls</td><td>Abandoned calls</td><td>Average 
 hold time</td><td>Average talk time</td></tr>";
-      foreach(explode("\n", $result['data']) as $line)
-      {
-         if (preg_match("/talktime/i", $line)) {
-          echo "<tr>";
-          $pieces = explode(" ", $line);
-          echo "<td class='large';>$pieces[0] </td>";
-          echo "<td class='large';>$pieces[2] </td> ";
-          echo "<td class='large';>".trim($pieces[14], "C:,")."</td> ";
-          echo "<td class='large';>".trim($pieces[15], "A:,")."</td> ";
-          echo "<td class='large';>".trim($pieces[9], "(s")." s</td> ";
-          echo "<td class='large';>".trim($pieces[11], "s")." s</td> ";
-          echo "</tr>";
-         }
-      }
-      echo "</table>";
-    }
+		foreach(explode("\n", $result['data']) as $line) {
+			if (preg_match("/talktime/i", $line)) {
+				echo "<tr>";
+				$pieces = explode(" ", $line);
+				echo "<td class='large';>$pieces[0] </td>";
+				echo "<td class='large';>$pieces[2] </td> ";
+				echo "<td class='large';>".trim($pieces[14], "C:,")."</td> ";
+				echo "<td class='large';>".trim($pieces[15], "A:,")."</td> ";
+				echo "<td class='large';>".trim($pieces[9], "(s")." s</td> ";
+				echo "<td class='large';>".trim($pieces[11], "s")." s</td> ";
+				echo "</tr>";
+			}
+		}
+		echo "</table>";
+	}
+
+	// Create two columns
+	echo "<br /><table border='0'>";
+	echo "<tr><td width=500px;>";
 
 
+	// Create two columns
+	echo "<br /><table border='0'>";
+	echo "<tr><td width=600px;>";
 
-// Create two columns
-echo "<br /><table border='0'>";
-echo "<tr><td width=500px;>";
+	// ECHO AGENTS AVAILABLE
+	echo "<h3><u>Agents logged in</u></h3>";
 
-
-// Create two columns
-echo "<br /><table border='0'>";
-echo "<tr><td width=600px;>";
-
-// ECHO AGENTS AVAILABLE
-
-   echo "<h3><u>Agents logged in</u></h3>";
-
-    if(!strpos($result['data'], ':'))
-      echo $peer['data'];
-    else
-    {
-      $data = array();
-      echo "<table border='1'; cellpadding=6pt;>";
-      echo "<tr class='heading-medium';><td>Agent ID</td><td>Status</td><td>Last call taken (seconds ago)</td><td>Calls today</td></tr>";
-      foreach(explode("\n", $result['data']) as $line)
-      {
-        if (preg_match("/      .*has taken \d{0,2} calls|has taken no calls yet/i", $line)) {
-          $pieces2 = preg_split('/\)\s\(|\s\(|\)\s/i',  trim(substr($line, 0, -1)));
-          echo "<tr>";
-          echo '<td class="medium">' . $pieces2[0] . '</td>';
-          echo '<td class="medium">' . $pieces2[4] . '</td>';          
-          echo '<td class="medium">' . preg_replace("/[^0-9]/","", $pieces2[6]) .' </td> ';
-          echo '<td class="medium">' . preg_replace("/[^0-9]/","", $pieces2[5]) .' </td> ';
-          echo "</tr>";
-        }
-      }
-      echo "</table>";
-    }
+	if(!strpos($result['data'], ':')) {
+		echo $peer['data'];
+	} else {
+		$data = array();
+		echo "<table border='1'; cellpadding=6pt;>";
+		echo "<tr class='heading-medium';><td>Agent ID</td><td>Status</td><td>Last call taken (seconds ago)</td><td>Calls today</td></tr>";
+		foreach(explode("\n", $result['data']) as $line) {
+			if (preg_match("/      .*has taken \d{0,2} calls|has taken no calls yet/i", $line)) {
+				$pieces2 = preg_split('/\)\s\(|\s\(|\)\s/i',  trim(substr($line, 0, -1)));
+				echo "<tr>";
+				echo '<td class="medium">' . $pieces2[0] . '</td>';
+				echo '<td class="medium">' . $pieces2[4] . '</td>';          
+				echo '<td class="medium">' . preg_replace("/[^0-9]/","", $pieces2[6]) .' </td> ';
+				echo '<td class="medium">' . preg_replace("/[^0-9]/","", $pieces2[5]) .' </td> ';
+				echo "</tr>";
+			}
+		}
+		echo "</table>";
+	}
 
 echo "</td><td>";
-
 echo "</td></tr></table>";
     $asm->disconnect();
-  }
+}
 ?>
 <img src="trv.jpg">
 </center>
---
+++ page.queuestats.php
@@ -0,0 +1,2 @@
+<iframe width=100% height=550px src="modules/queuestats/index.php"/>
+</iframe>
